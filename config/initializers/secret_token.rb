987b8c8c54b754a9c8e2eea851d1a4470ebf79cf
randee <randeeshen@gmail.com>
2013-07-24 15:17:15 +0800

    Initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xiangshu::Application.config.secret_token = '557b760b2222bafea3e34ff7d63d748979795a8ad67f1bc540eef0631458525961cb9d53a25629c581618efaa29dcbbcf7489df2a96e8651c275d92707879fc8'
